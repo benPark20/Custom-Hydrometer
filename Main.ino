0ad029b5c6cff1aa13c7842c409ad982dbe738d5
Ben Park <152503119+benPark20@users.noreply.github.com>
2024-08-05 11:21:34 -0400

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -13,7 +13,8 @@ DHT dht(DHTPIN, DHTTYPE);
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
+//settign up rgb pins
+const RGBPins [] = {16, 17, 18, 19, 20, 21, 26, 27}
 //global variables
 float lastH;
 float lastT;
@@ -22,7 +23,20 @@ void setup() {
   Serial1.begin(9600);
   Serial1.println("Hello, Raspberry Pi Pico W!");
   dht.begin();
+  for( int i=0; i<9; i++)
+    pinMode(RGBPins[i], OUTPUT);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
+  display.clearDisplay();
+  display.display();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(F("Booting..."));
+  display.display();
+  delay(2000);
+  display.setCursor(0, 20);
+  display.println(F("Done!"));
+  display.display();
 }
 void loop() {
   // put your main code here, to run repeatedly:
@@ -34,7 +48,8 @@ void loop() {
   if (isnan(h) || isnan(t)) {
     Serial1.println(F("Failed to read from DHT sensor!"));
     display.clearDisplay();
-    display.println(F("ERROR"));
+    display.println(F("ERROR 1"));
+    display.display();
   return;
   }
   if(lastH != h || lastT != t){
@@ -45,16 +60,29 @@ void loop() {
     Serial1.println(h);
     Serial1.print(F("Tempurature: "));
     Serial1.println(t);
+    
+    //show humidity on LEDs
+    if (h < 30){
+      analogWrite(RGBPins[17], 255);
+    }
+  
     // display contitions
     display.clearDisplay();
-    display.setTextSize(6);
-    display.setCursor(10,10);
-    display.print(h);
+    display.display();
+    display.setTextSize(4);
+    display.setCursor(7,7);
+    char hStr[6];
+    dtostrf(h, 4, 1, hStr);
+    display.print(hStr);
     display.println(F("%"));
-    display.setCursor(50,10);
+    display.setCursor(20,47);
     display.setTextSize(2);
     display.print(t);
-    display.println(F("Deg"));
+    display.println(F(" F"));
+    display.setTextSize(0);
+    display.setCursor(86,44);
+    display.print("o");
+    display.display();
   }
   else {
     return;
